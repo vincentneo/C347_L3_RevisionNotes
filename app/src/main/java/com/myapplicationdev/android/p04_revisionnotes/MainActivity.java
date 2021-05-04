16713e639ad17bfba41cdfb25c1b11b7b91c2b10
Zeniahacks <19023702@rp.edu.sg>
2021-05-04 15:28:18 +0800

    intent and buttons

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -1,13 +1,18 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Button buttonInsertNote, buttonShowList;
@@ -38,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
 
+        });
+
+        buttonShowList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create the DBHelper object, passing in the activity's content
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
         });
 
     }
