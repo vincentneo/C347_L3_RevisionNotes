b3d883ca2349e1777d9ea1606de3d25b64b0c7e9
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-05-04 14:05:19 +0800

    implement Note.java

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/Note.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/Note.java
@@ -1,7 +1,26 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
 public class Note {
+    
+    private int id, stars;
+    private String noteContent;
 
-	//What's here?
+    public Note(int id, String noteContent, int stars) {
+        this.id = id;
+        this.stars = stars;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public int getStars() {
+        return stars;
+    }
 
 }
