062934c1d5e2826972205236d00c6785629f94d3
fattahyousaini <50009909+fattahyousaini@users.noreply.github.com>
2021-05-04 16:11:10 +0800

    Done

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
@@ -1,14 +1,32 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class SecondActivity extends AppCompatActivity {
+	ListView lvData;
+
+	RevisionNotesArrayAdapter listAdapter;
+	ArrayAdapter aa;
+	ArrayList<Note> note;
+	DBHelper dbTask;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//TODO implement the Custom ListView
+		setContentView(R.layout.activity_second);
+
+		lvData = (ListView) this.findViewById(R.id.lv);
+		DBHelper db = new DBHelper(SecondActivity.this);
+		note = db.getAllNotes();
+		db.close();
+
+		aa= new RevisionNotesArrayAdapter(this, R.layout.row, note);
+		lvData.setAdapter(aa);
 
 	}
 
