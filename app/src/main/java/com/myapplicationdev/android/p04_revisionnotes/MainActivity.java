c134b249c35448168548de0e027c6c5ae0943b04
Zeniahacks <19023702@rp.edu.sg>
2021-05-04 15:14:45 +0800

    Main

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -2,12 +2,43 @@ package com.myapplicationdev.android.p04_revisionnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonInsertNote, buttonShowList;
+    EditText editTextNote;
+    RadioGroup radioGroupStars;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonInsertNote = findViewById(R.id.buttonInsertNote);
+        buttonShowList = findViewById(R.id.buttonShowList);
+        editTextNote = findViewById(R.id.editTextNote);
+        radioGroupStars = findViewById(R.id.radioGroupStars);
+
+        buttonInsertNote.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertNote(editTextNote.getText().toString(), radioGroupStars.getCheckedRadioButtonId());
+                db.close();
+                
+                Toast.makeText(MainActivity.this, "Inserted",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        });
+
     }
 }
